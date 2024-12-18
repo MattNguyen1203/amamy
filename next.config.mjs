6f8cb8f495b6913625ab01cf297e6696cf1be7a0
FinnTVD <finnit.th@gmail.com>
2024-12-18 09:46:23 +0700

    init project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createNextIntlPlugin from 'next-intl/plugin'
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin()
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    formats: ['image/webp'],
+    minimumCacheTTL: 3600,
+    remotePatterns: [
+      {protocol: 'https', hostname: '**'},
+      {protocol: 'http', hostname: '**'},
+    ],
+  },
+  reactStrictMode: false,
+  output: 'standalone',
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+}
+
+export default withNextIntl(nextConfig)
