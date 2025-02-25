0ae73f6773a89b816b0c3bfa43a46b077ceffa27
minh6825 <minhnqdeveloper@gmail.com>
2025-02-25 23:44:07 +0700

    Add sitemap generation and update ESLint configuration

--- src/app/(sitemap)/home/sitemap.ts
+++ src/app/(sitemap)/home/sitemap.ts
@@ -3,4 +3,28 @@
 
 export default async function sitemap() {
   
+    return {
+        pages: [
+        {
+            url: '/',
+            changefreq: 'daily',
+            priority: 1,
+        },
+        {
+            url: '/about',
+            changefreq: 'daily',
+            priority: 0.9,
+        },
+        {
+            url: '/contact',
+            changefreq: 'daily',
+            priority: 0.8,
+        },
+        {
+            url: '/products',
+            changefreq: 'daily',
+            priority: 0.7,
+        },
+        ],
+    }
 }
