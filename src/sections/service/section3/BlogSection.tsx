01c1ac4d09f221a93d0a305b0d3cea0d0af70c9d
minhlh <minhhl298.st@gmail.com>
2025-03-03 22:02:28 +0700

    feat: service page

--- src/sections/service/section3/BlogSection.tsx
+++ src/sections/service/section3/BlogSection.tsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import SlideDocs from './SlideDocs'
 
-type Props = {}
-
-const Section6 = (props: Props) => {
+const BlogSection = () => {
   return (
-    <div className='flex pt-24 pl-24 pb-24 flex-col items-start gap-2.5 self-stretch w-full xsm:pl-[1rem]'>
+    <div className='flex pt-24 pl-24 pb-24 flex-col items-start gap-2.5 self-stretch w-full xsm:pl-[1rem] bg-[#EDF5FA]'>
       <div className='flex justify-between w-full flex-col'>
         <div className='flex  justify-between w-full mb-[2.5rem] xsm:items-center'>
           <h2 className='text-[2.875rem] not-italic font-bold leading-[120%] xsm:text-[1.375rem]'>
             Các tin tức mới nhất
           </h2>
           <button
-            className='flex rounded-[0.5rem] mr-24 xsm:mr-[1rem] xsm:rounded-full xsm:w-[2.66rem] xsm:flex-shrink-0 xsm:h-[2.66rem] xsm:p-0 text-white h-12 pl-6 pr-4 py-3 
+            className='flex rounded-[1.25rem] mr-24 xsm:mr-[1rem] xsm:rounded-full xsm:w-[2.66rem] xsm:flex-shrink-0 xsm:h-[2.66rem] xsm:p-0 text-white h-12 pl-6 pr-4 py-3 
             justify-center items-center gap-1 rounded-tl-[var(--8,] 
             rounded-br-[var(--8,] rounded-tr-[0.5rem)] rounded-bl-[0.5rem)] border-[1.5px] border-solid border-[rgba(255,255,255,0.80)] 
             bg-[var(--Blue-Primary,_#38B6FF)]'
@@ -27,7 +25,7 @@ const Section6 = (props: Props) => {
   )
 }
 
-export default Section6
+export default BlogSection
 
 const ArrowIcon = () => {
   return (
