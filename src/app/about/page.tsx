110199837601b299f4eacbf0f2724cb4db209ce8
minhlh <minhhl298.st@gmail.com>
2025-03-04 22:33:18 +0700

    feat about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import AboutPage from '@/sections/about'
+
+export default async function About() {
+  return (
+    <div className='w-full bg-white text-black flex flex-col items-center'>
+      <AboutPage />
+    </div>
+  )
+}
