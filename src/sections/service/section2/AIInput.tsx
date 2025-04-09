c1967b9c63c491ac1e985824f13ee018a29cc99c
Đỗ Công Sơn <sonanhson99@gmail.com>
2025-04-09 16:10:59 +0700

    refactor: update Header and AIInput components for improved styling and structure

--- src/sections/service/section2/AIInput.tsx
+++ src/sections/service/section2/AIInput.tsx
@@ -30,14 +30,16 @@ const AIInput = ({
       />
       <div className='w-[calc(100%+0.25rem)] h-[calc(100%+0.25rem)] -top-0.5 -left-0.5 absolute rounded-[1.3125rem] opacity-30 bg-[linear-gradient(90deg,#F26AFF_0%,#9E65FF_21.28%,#436EFF_100%)] shadow-[0px_0.5px_2.1px_0px_rgba(255,255,255,0.25)]' />
       <div className='relative z-10 flex items-center rounded-[1.25rem] bg-[linear-gradient(92deg,#F26AFF_81.16%,#9E65FF_84.89%,#436EFF_98.7%)]'>
-        <input
-          disabled={disabled}
-          type='text'
-          placeholder={placeholder || 'Nhập yêu cầu của bạn...'}
-          value={value}
-          onChange={(e) => onChange?.(e.target.value)}
-          className='disabled:bg-white flex-1 p-4 border border-Blue-100 outline-none rounded-[1.25rem] text-pc-sub14m text-black placeholder:text-black/30'
-        />
+        <div className='bg-white flex-1 border border-Blue-100 outline-none rounded-[1.25rem]'>
+          <input
+            disabled={disabled}
+            type='text'
+            placeholder={placeholder || 'Nhập yêu cầu của bạn...'}
+            value={value}
+            onChange={(e) => onChange?.(e.target.value)}
+            className='disabled:bg-white p-4 border-none outline-none rounded-[1.25rem] text-pc-sub14m text-black placeholder:text-black/30'
+          />
+        </div>
         <button
           className={cn(
             'flex-center pr-[0.875rem]',
