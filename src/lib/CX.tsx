a14569ae575378e48aa1d202d44cc60996c52705
Matt <huongax123@gmail.com>
2025-04-24 11:37:18 +0700

    update icon AI

---
+++ src/lib/CX.tsx
@@ -0,0 +1,14 @@
+import Script from 'next/script'
+import React from 'react'
+
+const CX = () => {
+  return (
+    <Script
+      src='https://widget.cxgenie.ai/widget.js'
+      data-aid='685cebfd-06e3-4c87-9b90-c091ab4eb7c3'
+      data-lang='vi'
+    ></Script>
+  )
+}
+
+export default CX
