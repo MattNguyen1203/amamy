bef7ec705ac17e21eec8c8a024e5fe020c16b3f6
longtrq26 <longqtr2608@gmail.com>
2025-09-10 10:40:09 +0700

    refactor(amamy page): refactor fetch data option

--- src/app/(baselayout)/amamy/page.tsx
+++ src/app/(baselayout)/amamy/page.tsx
@@ -1,18 +1,45 @@
 import React from 'react'
 import fetchData from '@/fetch/fetchData'
+import getMetaDataRankMath from '@/fetch/getMetaDataRankMath'
 import FAQ from '@/sections/amamy/faq/FAQ'
 import Hero from '@/sections/amamy/hero/Hero'
 import Process from '@/sections/amamy/process/Process'
 import Reason from '@/sections/amamy/reason/Reason'
 import Banner from '@/sections/homepage/banner'
 import Section6 from '@/sections/homepage/section6/Section6'
+import metadataValues from '@/utils/metadataValues'
+
+export async function generateMetadata() {
+  const res = await getMetaDataRankMath('')
+  return metadataValues(res)
+}
 
 const page = async () => {
-  const fetchBanner = fetchData({api: 'pages/11'})
-  const fetchChatBoxAI = fetchData({api: 'options?fields=box_chat_ai'})
-  const fetchDeliveryDirection = fetchData({api: 'chieu-van-chuyen'})
+  const fetchBanner = fetchData({
+    api: 'pages/11',
+    option: {
+      next: {revalidate: 60},
+    },
+  })
+  const fetchChatBoxAI = fetchData({
+    api: 'options?fields=box_chat_ai',
+    option: {
+      next: {revalidate: 60},
+    },
+  })
+  const fetchDeliveryDirection = fetchData({
+    api: 'chieu-van-chuyen',
+    method: 'GET',
+    option: {
+      next: {revalidate: 60},
+    },
+  })
   const fetchCurrencyExchangeRate = fetchData({
     api: 'options?fields=currency_to_usd',
+    method: 'GET',
+    option: {
+      next: {revalidate: 60},
+    },
   })
 
   const [
